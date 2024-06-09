7afe8faa5df26c0c321071617159eba697b0b255
mireu-lab <limmireu1214@gmail.com>
2024-06-09 18:45:12 +0900

    update 2 files, rename 1 file, create 4 files and delete 1 file

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -14,6 +13,252 @@ import (
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
 )
+
+// WriteJointRotations writes joint rotation data to a CSV file.
+func WriteJointRotations(bvhTree *BvhTree, filePath string, endSites bool) bool {
+	startTime := time.Now()
+
+	timeCol := make([]float64, bvhTree.NFrames())
+	for i := 0; i < bvhTree.NFrames(); i++ {
+		timeCol[i] = float64(i) * bvhTree.FrameTime()
+	}
+
+	data := make([][]float64, len(bvhTree.GetJoints(endSites)))
+	header := []string{"time"}
+
+	for i, joint := range bvhTree.GetJoints(endSites) {
+		channels := bvhTree.JointChannels(joint.Name())
+		rotationChannels := make([]string, 0)
+
+		for _, channel := range channels {
+			if strings.HasSuffix(channel, "rotation") {
+				rotationChannels = append(rotationChannels, channel)
+			}
+		}
+		JointChannels := bvhTree.FramesJointChannels(joint.Name(), rotationChannels, 0.0)[i]
+
+		header = append(header, fmt.Sprintf("%s.%s", joint.Name(), strings.ToLower(string(rotationChannels[0][0]))))
+		data[i] = append(data[i], timeCol...)
+		data[i] = append(data[i], JointChannels...)
+	}
+
+	endTime := time.Now()
+	fmt.Printf("rotations: %s\n", endTime.Sub(startTime))
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Printf("ERROR(%s): Could not write to file %s.\nMake sure you have writing permissions.\n", err, filePath)
+		return false
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	fmt.Fprintf(writer, "%s\n", strings.Join(header, ","))
+	for _, row := range data {
+		for j, v := range row {
+			if j > 0 {
+				fmt.Fprintf(writer, ",")
+			}
+			fmt.Fprintf(writer, "%10.5f", v)
+		}
+		fmt.Fprintf(writer, "\n")
+	}
+
+	return true
+}
+
+// WriteJointPositions writes joint world position data to a CSV file.
+func WriteJointPositions(bvhTree *BvhTree, filePath string, scale float64, endSites bool) bool {
+	startTime := time.Now()
+
+	timeCol := make([]float64, bvhTree.NFrames())
+	for i := 0; i < bvhTree.NFrames(); i++ {
+		timeCol[i] = float64(i) * bvhTree.FrameTime()
+	}
+
+	data := make([][]float64, len(bvhTree.GetJoints(true)))
+	header := []string{"time"}
+
+	var getWorldPositions func(joint *BvhNode)
+	getWorldPositions = func(joint *BvhNode) {
+		var offset []float64
+		if joint.Value[0] == "End" {
+			offset = []float64{0, 0, 0}
+		} else {
+			offset = bvhTree.JointOffset(joint.Name())
+		}
+		for _, axis := range []string{"x", "y", "z"} {
+			header = append(header, fmt.Sprintf("%s.%s", joint.Name(), axis))
+		}
+
+		fmt.Println(bvhTree.NFrames())
+
+		for i := 0; i < bvhTree.NFrames(); i++ {
+			fmt.Println(joint.Name())
+			fmt.Println(bvhTree.GetJointIndex(joint.Name()))
+
+			data[bvhTree.GetJointIndex(joint.Name())] = append(data[bvhTree.GetJointIndex(joint.Name())], timeCol[i])
+			data[bvhTree.GetJointIndex(joint.Name())] = append(data[bvhTree.GetJointIndex(joint.Name())], scale*offset[0])
+			data[bvhTree.GetJointIndex(joint.Name())] = append(data[bvhTree.GetJointIndex(joint.Name())], scale*offset[1])
+			data[bvhTree.GetJointIndex(joint.Name())] = append(data[bvhTree.GetJointIndex(joint.Name())], scale*offset[2])
+		}
+		for _, child := range bvhTree.JointChildren(joint.Name()) {
+			if endSites {
+				getWorldPositions(child)
+			}
+			if child.Value[0] == "JOINT" {
+				getWorldPositions(child)
+			}
+		}
+	}
+	getWorldPositions(bvhTree.Search("ROOT")[0])
+
+	endTime := time.Now()
+	fmt.Printf("positions: %s\n", endTime.Sub(startTime))
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Printf("ERROR(%s): Could not write to file %s.\nMake sure you have writing permissions.\n", err, filePath)
+		return false
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	fmt.Fprintf(writer, "%s\n", strings.Join(header, ","))
+	for _, row := range data {
+		for j, v := range row {
+			if j > 0 {
+				fmt.Fprintf(writer, ",")
+			}
+			fmt.Fprintf(writer, "%10.5f", v)
+		}
+		fmt.Fprintf(writer, "\n")
+	}
+
+	return true
+}
+
+// WriteJointHierarchy writes joint hierarchy data to a CSV file.
+func WriteJointHierarchy(bvhTree *BvhTree, filePath string, scale float64) bool {
+	startTime := time.Now()
+
+	var data [][]string
+	for _, joint := range bvhTree.GetJoints(true) {
+		jointName := joint.Name()
+		parentName := ""
+		if bvhTree.JointParent(jointName) != nil {
+			parentName = bvhTree.JointParent(jointName).Name()
+		}
+		offset := bvhTree.JointOffset(jointName)
+		row := []string{jointName, parentName, fmt.Sprintf("%f", scale*offset[0]), fmt.Sprintf("%f", scale*offset[1]), fmt.Sprintf("%f", scale*offset[2])}
+		data = append(data, row)
+	}
+
+	endTime := time.Now()
+	fmt.Printf("hierarchy: %s\n", endTime.Sub(startTime))
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		fmt.Printf("ERROR(%s): Could not write to file %s.\nMake sure you have writing permissions.\n", err, filePath)
+		return false
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	fmt.Fprintf(writer, "joint,parent,offset.x,offset.y,offset.z\n")
+	for _, row := range data {
+		fmt.Fprintf(writer, "%s,%s,%s,%s,%s\n", row[0], row[1], row[2], row[3], row[4])
+	}
+
+	return true
+}
+
+// Bvh2Csv converts a BVH file to CSV format.
+func Bvh2Csv(bvhPath string, dstDirpath string, scale float64, exportRotation bool, exportPosition bool, exportHierarchy bool, endSites bool) bool {
+	startTime := time.Now()
+
+	var file *os.File
+	var err error
+	if file, err = os.Open(bvhPath); err != nil {
+		fmt.Printf("ERROR %s: Could not open file %s\n", err, bvhPath)
+		return false
+	}
+	defer file.Close()
+
+	data, _ := io.ReadAll(file)
+	bvhTree := NewBvhTree(string(data))
+
+	endTime := time.Now()
+	fmt.Printf("file read: %s\n", endTime.Sub(startTime))
+
+	var posSuccess, rotSuccess, hierarchySuccess bool
+	posSuccess = false
+	rotSuccess = false
+	hierarchySuccess = false
+
+	if exportPosition {
+		posSuccess = WriteJointPositions(bvhTree, fmt.Sprintf("%s/%s_pos.csv", dstDirpath, bvhPath), scale, endSites)
+	}
+
+	if exportRotation {
+		rotSuccess = WriteJointRotations(bvhTree, fmt.Sprintf("%s/%s_rot.csv", dstDirpath, bvhPath), endSites)
+	}
+
+	if exportHierarchy {
+		hierarchySuccess = WriteJointHierarchy(bvhTree, fmt.Sprintf("%s/%s_hierarchy.csv", dstDirpath, bvhPath), scale)
+	}
+
+	return bool(posSuccess && rotSuccess && hierarchySuccess)
+}
+
+// PlotCsv plots data from a CSV file.
+func PlotCsv(filePath string, xColumn int, yColumn int) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // Skip header line
+	header := strings.Split(scanner.Text(), ",")
+
+	var pts plotter.XYs
+	for scanner.Scan() {
+		row := strings.Split(scanner.Text(), ",")
+		x, err := strconv.ParseFloat(row[xColumn], 64)
+		if err != nil {
+			return err
+		}
+		y, err := strconv.ParseFloat(row[yColumn], 64)
+		if err != nil {
+			return err
+		}
+		pts = append(pts, plotter.XY{X: x, Y: y})
+	}
+
+	p := plot.New()
+	s, err := plotter.NewScatter(pts)
+	if err != nil {
+		return err
+	}
+	p.Add(s)
+
+	p.X.Label.Text = header[xColumn]
+	p.Y.Label.Text = header[yColumn]
+
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, "plot.png"); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // BvhNode represents a node in a BVH hierarchy.
 type BvhNode struct {
@@ -173,8 +418,8 @@ func (b *Bvh) JointDirectChildren(name string) []*BvhNode {
 
 // GetJointIndex returns the index of a joint in the list of joints.
 func (b *Bvh) GetJointIndex(name string) int {
-	for i, joint := range b.GetJoints() {
-		if joint.Name() == name {
+	for i, jointName := range b.GetJointsNames() {
+		if jointName == name {
 			return i
 		}
 	}
@@ -231,7 +476,7 @@ func (b *Bvh) GetJointChannelIndex(joint string, channel string) int {
 			return i
 		}
 	}
-	return -1
+	return -1 // Not found
 }
 
 // FrameJointChannel returns the value of a channel for a joint at a given frame.
@@ -401,12 +646,16 @@ func (bt *BvhTree) GetJoint(name string) *BvhNode {
 
 // GetJointIndex returns the index of a joint in the list of joints, including End Sites.
 func (bt *BvhTree) GetJointIndex(name string) int {
-	for i, joint := range bt.GetJoints(true) {
-		if joint.Name() == name {
+	joint := bt.GetJoint(name)
+	if joint.Parent == bt.Root {
+		return -1
+	}
+	for i, parentCandidate := range bt.GetJoints(true) { // Get all joints, including End Sites
+		if parentCandidate == joint.Parent {
 			return i
 		}
 	}
-	return -1 // Not found
+	return -1 // Should never happen if the parent exists
 }
 
 // JointChildren returns the direct children of a joint, including End Sites.
@@ -420,18 +669,6 @@ func (bt *BvhTree) JointChildren(name string) []*BvhNode {
 		children = append(children, child)
 	}
 	return children
-}
-
-// WriteFile writes the BVH data to a file.
-func (bt *BvhTree) WriteFile(filePath string) bool {
-	file, err := os.Create(filePath)
-	if err != nil {
-		fmt.Println("ERROR:", err)
-		return false
-	}
-	defer file.Close()
-	bt.Write(file)
-	return true
 }
 
 // Write writes the BVH data to the given stream.
@@ -448,7 +685,7 @@ func (bt *BvhTree) GetHierarchyString() string {
 		s = bt.CloseScopes(s, bt.GetJointDepth(joint.Name()))
 		s += bt.GetJointString(joint)
 	}
-	s = bt.CloseScopes(s, 0) // Close all remaining scopes at the end
+	s = bt.CloseScopes(s, 0)
 	return s
 }
 
@@ -505,271 +742,58 @@ func (bt *BvhTree) GetMotionString() string {
 	return s
 }
 
-// WriteJointRotations writes joint rotation data to a CSV file.
-func WriteJointRotations(bvhTree *BvhTree, filePath string, endSites bool) bool {
-	startTime := time.Now()
-
-	timeCol := make([]float64, bvhTree.NFrames())
-	for i := 0; i < bvhTree.NFrames(); i++ {
-		timeCol[i] = float64(i) * bvhTree.FrameTime()
-	}
-
-	data := make([][]float64, len(bvhTree.GetJoints(endSites)))
-	header := []string{"time"}
-
-	for i, joint := range bvhTree.GetJoints(true) {
-		channels := bvhTree.JointChannels(joint.Name())
-		rotationChannels := make([]string, 0)
-		for _, channel := range channels {
-			if strings.HasSuffix(channel, "rotation") {
-				rotationChannels = append(rotationChannels, channel)
-			}
+// contains returns true if the given slice contains the given element.
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
 		}
-		header = append(header, fmt.Sprintf("%s.%s", joint.Name(), strings.ToLower(string(rotationChannels[0][0]))))
-		data[i] = append(data[i], timeCol...)
-
-		// Flatten the [][]float64 to []float64:
-		flattenedData := make([]float64, 0)
-		for _, frame := range bvhTree.FramesJointChannels(joint.Name(), rotationChannels, 0.0) {
-			flattenedData = append(flattenedData, frame...)
-		}
-
-		data[i] = append(data[i], flattenedData...)
 	}
-
-	endTime := time.Now()
-	fmt.Printf("rotations: %s\n", endTime.Sub(startTime))
-
-	file, err := os.Create(filePath)
-	if err != nil {
-		fmt.Printf("ERROR(%s): Could not write to file %s.\nMake sure you have writing permissions.\n", err, filePath)
-		return false
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	defer writer.Flush()
-
-	fmt.Fprintf(writer, "%s\n", strings.Join(header, ","))
-	for _, row := range data {
-		for j, v := range row {
-			if j > 0 {
-				fmt.Fprintf(writer, ",")
-			}
-			fmt.Fprintf(writer, "%10.5f", v)
-		}
-		fmt.Fprintf(writer, "\n")
-	}
-
-	return true
+	return false
 }
 
-// WriteJointPositions writes joint world position data to a CSV file.
-func WriteJointPositions(bvhTree *BvhTree, filePath string, scale float64, endSites bool) bool {
-	startTime := time.Now()
-
-	timeCol := make([]float64, bvhTree.NFrames())
-	for i := 0; i < bvhTree.NFrames(); i++ {
-		timeCol[i] = float64(i) * bvhTree.FrameTime()
-	}
-
-	data := make([][]float64, len(bvhTree.GetJoints(true)))
-	header := []string{"time"}
-
-	var getWorldPositions func(joint *BvhNode)
-	getWorldPositions = func(joint *BvhNode) {
-		var offset []float64
-
-		if joint.Value[0] == "End" {
-			offset = []float64{0, 0, 0}
-
-		} else {
-			// Correctly use bvhTree.JointOffset:
-			offset = bvhTree.JointOffset(joint.Name())
-		}
-
-		for _, axis := range []string{"x", "y", "z"} {
-			header = append(header, fmt.Sprintf("%s.%s", joint.Name(), axis))
-		}
-
-		for i := 0; i < bvhTree.NFrames(); i++ {
-			data[bvhTree.GetJointIndex(joint.Name())] = append(data[bvhTree.GetJointIndex(joint.Name())], timeCol[i])
-			data[bvhTree.GetJointIndex(joint.Name())] = append(data[bvhTree.GetJointIndex(joint.Name())], scale*offset[0])
-			data[bvhTree.GetJointIndex(joint.Name())] = append(data[bvhTree.GetJointIndex(joint.Name())], scale*offset[1])
-			data[bvhTree.GetJointIndex(joint.Name())] = append(data[bvhTree.GetJointIndex(joint.Name())], scale*offset[2])
-		}
-
-		for _, child := range bvhTree.JointChildren(joint.Name()) {
-			if endSites {
-				getWorldPositions(child)
-			}
-			if child.Value[0] == "JOINT" {
-				getWorldPositions(child)
-			}
+// index returns the index of the given element in the given slice.
+func index(s []string, e string) int {
+	for i, a := range s {
+		if a == e {
+			return i
 		}
 	}
-	getWorldPositions(bvhTree.Search("ROOT")[0])
-
-	endTime := time.Now()
-	fmt.Printf("positions: %s\n", endTime.Sub(startTime))
-
-	file, err := os.Create(filePath)
-	if err != nil {
-		fmt.Printf("ERROR(%s): Could not write to file %s.\nMake sure you have writing permissions.\n", err, filePath)
-		return false
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	defer writer.Flush()
-
-	fmt.Fprintf(writer, "%s\n", strings.Join(header, ","))
-	for _, row := range data {
-		for j, v := range row {
-			if j > 0 {
-				fmt.Fprintf(writer, ",")
-			}
-			fmt.Fprintf(writer, "%10.5f", v)
-		}
-		fmt.Fprintf(writer, "\n")
-	}
-
-	return true
-}
-
-// WriteJointHierarchy writes joint hierarchy data to a CSV file.
-func WriteJointHierarchy(bvhTree *BvhTree, filePath string, scale float64) bool {
-	startTime := time.Now()
-
-	var data [][]string
-	for _, joint := range bvhTree.GetJoints(true) {
-		jointName := joint.Name()
-		parentName := ""
-		if bvhTree.JointParent(jointName) != nil {
-			parentName = bvhTree.JointParent(jointName).Name()
-		}
-		offset := bvhTree.JointOffset(jointName)
-		row := []string{jointName, parentName, fmt.Sprintf("%f", scale*offset[0]), fmt.Sprintf("%f", scale*offset[1]), fmt.Sprintf("%f", scale*offset[2])}
-		data = append(data, row)
-	}
-
-	endTime := time.Now()
-	fmt.Printf("hierarchy: %s\n", endTime.Sub(startTime))
-
-	file, err := os.Create(filePath)
-	if err != nil {
-		fmt.Printf("ERROR(%s): Could not write to file %s.\nMake sure you have writing permissions.\n", err, filePath)
-		return false
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	defer writer.Flush()
-
-	fmt.Fprintf(writer, "joint,parent,offset.x,offset.y,offset.z\n")
-	for _, row := range data {
-		fmt.Fprintf(writer, "%s,%s,%s,%s,%s\n", row[0], row[1], row[2], row[3], row[4])
-	}
-
-	return true
-}
-
-// Bvh2Csv converts a BVH file to CSV format.
-func Bvh2Csv(bvhPath string, dstDirpath string, scale float64, exportRotation bool, exportPosition bool, exportHierarchy bool, endSites bool) bool {
-	startTime := time.Now()
-	path, _ := filepath.Abs(bvhPath)
-
-	var file *os.File
-	var err error
-	if file, err = os.Open(bvhPath); err != nil {
-		fmt.Printf("ERROR %s: Could not open file %s\n", err, bvhPath)
-		return false
-	}
-	defer file.Close()
-
-	data, _ := io.ReadAll(file)
-	bvhTree := NewBvhTree(string(data))
-
-	endTime := time.Now()
-	fmt.Printf("file read: %s\n", endTime.Sub(startTime))
-
-	var posSuccess, rotSuccess, hierarchySuccess bool
-	posSuccess = false
-	rotSuccess = false
-	hierarchySuccess = false
-
-	if exportPosition {
-		posSuccess = WriteJointPositions(bvhTree, fmt.Sprintf("%s/%s_pos.csv", dstDirpath, path), scale, endSites)
-	}
-
-	if exportRotation {
-		rotSuccess = WriteJointRotations(bvhTree, fmt.Sprintf("%s/%s_rot.csv", dstDirpath, path), endSites)
-	}
-
-	if exportHierarchy {
-		hierarchySuccess = WriteJointHierarchy(bvhTree, fmt.Sprintf("%s/%s_hierarchy.csv", dstDirpath, path), scale)
-	}
-
-	return bool(posSuccess && rotSuccess && hierarchySuccess)
-}
-
-// PlotCsv plots data from a CSV file.
-func PlotCsv(filePath string, xColumn int, yColumn int) error {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan() // Skip header line
-	header := strings.Split(scanner.Text(), ",")
-
-	var pts plotter.XYs
-	for scanner.Scan() {
-		row := strings.Split(scanner.Text(), ",")
-		x, err := strconv.ParseFloat(row[xColumn], 64)
-		if err != nil {
-			return err
-		}
-		y, err := strconv.ParseFloat(row[yColumn], 64)
-		if err != nil {
-			return err
-		}
-		pts = append(pts, plotter.XY{X: x, Y: y})
-	}
-
-	p := plot.New()
-	s, err := plotter.NewScatter(pts)
-	if err != nil {
-		return err
-	}
-	p.Add(s)
-
-	p.X.Label.Text = header[xColumn]
-	p.Y.Label.Text = header[yColumn]
-
-	if err := p.Save(4*vg.Inch, 4*vg.Inch, "plot.png"); err != nil {
-		return err
-	}
-
-	return nil
+	return -1
 }
 
 func main() {
-	// Example usage:
-	bvhPath := "test/ex/PT_S104_1_FM_M_009.bvh"
-	dstDirpath := "test/csv"
+	// // Example usage:
+	// bvhData := ``
 
-	path, _ := filepath.Abs(bvhPath)
+	// // Create a new BvhTree from the BVH data.
+	// bvhTree := NewBvhTree(bvhData)
+
+	// // Print the joint names, including End Sites.
+	// fmt.Println("Joint Names:", bvhTree.GetJointsNames(true))
+
+	// // Print the number of frames.
+	// fmt.Println("Number of Frames:", bvhTree.NFrames())
+
+	// // Print the frame time.
+	// fmt.Println("Frame Time:", bvhTree.FrameTime())
+
+	// // Get the values of the Xrotation channel for the LeftHip joint for all frames.
+	// leftHipXRotation := bvhTree.FramesJointChannels("LeftHip", []string{"Xrotation"}, 0.0)
+	// fmt.Println("LeftHip Xrotation:", leftHipXRotation)
+
+	// Example usage:
+	bvhPath := "test/ex/test.bvh"
+	dstDirpath := "test/out"
 
 	// Convert BVH to CSV
 	if success := Bvh2Csv(bvhPath, dstDirpath, 1.0, true, true, true, true); success {
 		fmt.Println("BVH data converted to CSV successfully.")
 	}
 
-	// Plot CSV data (example: plot the x-position of the Hips joint)
-	if err := PlotCsv(fmt.Sprintf("%s/%s_pos.csv", dstDirpath, strings.TrimSuffix(path, ".bvh")), 0, 1); err != nil {
-		fmt.Println("Error plotting data:", err)
-	}
+	// // Plot CSV data (example: plot the x-position of the Hips joint)
+	// if err := PlotCsv(fmt.Sprintf("%s/%s_pos.csv", dstDirpath, strings.TrimSuffix(bvhPath, ".bvh")), 0, 1); err != nil {
+	// 	fmt.Println("Error plotting data:", err)
+	// }
+
 }
